bb4ee4f894ca5151d055865c21fdbc41780f76f8
Casper <stevenscasper@gmail.com>
2011-04-28 21:25:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schoolcatchmentarea::Application.config.secret_token = '6f5e38e0da47dcb3e960d044917f0d62a4c9ec8873d43ecb11155908ebce8b92f908a125960b0fd94e1f09d5ab082fbaee585f424b920c519e19956a8aa5cda2'
